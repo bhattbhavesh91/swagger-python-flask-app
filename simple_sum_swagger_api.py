77d349508db29b7a54118f7863f1c7a1d04af396
Bhavesh Bhatt <bhattbhavesh91@gmail.com>
2019-03-07 18:14:50 +0530

    v1.0.2.7.3.2019

--- simple_sum_swagger_api.py
+++ simple_sum_swagger_api.py
@@ -3,7 +3,7 @@ import json
 from flask import Flask, request, jsonify
 from flasgger import Swagger
 from flasgger.utils import swag_from
-
+from flasgger import LazyString, LazyJSONEncoder
 
 def add_2_numbers(num1,num2):
 	output = {'sum_of_numbers':0}
@@ -13,7 +13,34 @@ def add_2_numbers(num1,num2):
 
 
 app = Flask(__name__)
-swagger = Swagger(app)
+app.config['SWAGGER'] = {
+    'title': 'Swagger-UI',
+    'uiversion': 2
+}
+
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
+    "swagger_ui": True,
+    "specs_route": "/swagger/"
+}
+
+template = dict(swaggerUiPrefix=LazyString(
+    lambda: request.environ.get('HTTP_X_SCRIPT_NAME', '')))
+
+app.json_encoder = LazyJSONEncoder
+
+swagger = Swagger(app, config=swagger_config, template=template)
 
 
 @app.route("/")
